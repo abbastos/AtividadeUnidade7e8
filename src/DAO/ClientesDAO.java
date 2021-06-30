778f82daa4ee940b984f4ae4a779a1f732d5d334
Adriano Benelli Bastos <adriano.bbastos@gmail.com>
2021-06-29 21:13:34 -0300

    criando tela

---
+++ src/DAO/ClientesDAO.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import entidades.Cliente;
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author AdrianoBenelliBastos
+ */
+public class ClientesDAO {
+
+    public Boolean Cadastrar(Cliente obj) throws ClassNotFoundException {
+        Boolean retorno = false;
+
+        String sql = "INSERT INTO clientes (nome, rua, bairro, numero, cep, cidade, celular) "
+                + "VALUES (?,?,?,?,?,?,?)";
+        PreparedStatement pst = ConectaBancoDados.getPreparedStatement(sql);
+
+        try {
+
+            pst.setString(1, obj.getNome());
+            pst.setString(2, obj.getRua());
+            pst.setString(3, obj.getBairro());
+            pst.setString(4, obj.getNro());
+            pst.setString(5, obj.getCep());
+            pst.setString(6, obj.getCidade());
+            pst.setString(7, obj.getCelular());
+
+            Integer resultado = pst.executeUpdate();
+            if (resultado > 0) {
+                retorno = true;
+            } else {
+                retorno = false;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Erro ao acessar banco" + ex.getMessage().toLowerCase());
+        }
+
+        return retorno;
+
+    }
+}
---
+++ src/entidades/Cliente.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author AdrianoBenelliBastos
+ */
+public class Cliente {
+    
+    private String nome;
+    private String rua;
+    private String bairro;
+    private String nro;
+    private String cep;
+    private String cidade;
+    private String celular;
+
+     
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the rua
+     */
+    public String getRua() {
+        return rua;
+    }
+
+    /**
+     * @param rua the rua to set
+     */
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the nro
+     */
+    public String getNro() {
+        return nro;
+    }
+
+    /**
+     * @param nro the nro to set
+     */
+    public void setNro(String nro) {
+        this.nro = nro;
+    }
+
+    /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the celular
+     */
+    public String getCelular() {
+        return celular;
+    }
+
+    /**
+     * @param celular the celular to set
+     */
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    
+    
+    
+    
+}
